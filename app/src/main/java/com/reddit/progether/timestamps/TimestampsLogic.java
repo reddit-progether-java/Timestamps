ae81162be6fd1727b3c445fc1d8f83ccd02c863b
JeremyFunk <JeremyFunk@outlook.de>
2017-06-25 19:50:27 +0200

    Update TimestampsLogic
    Basic functions implemented

--- app/src/main/java/com/reddit/progether/timestamps/TimestampsLogic.java
+++ app/src/main/java/com/reddit/progether/timestamps/TimestampsLogic.java
@@ -1,16 +1,40 @@
 package Timestamps;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class TimestampsLogic {
-	
+
 	private static ArrayList<Timestamp> timestamps = new ArrayList<Timestamp>();
 	
-	public static void onButtonOpenClicked()
-	{
-		timestamps.add(new Timestamp(new java.sql.Date(System.currentTimeMillis()), TimestampStatus.Gestartet));
-	}public static void onButtonCloseClicked()
-	{
+	//true if the latest Timestamp is one wich was created by the onButtonStartClicked method
+	//used to avoid multiple start Timestamps in a row
+	public static boolean isStarted = false;
+	
+	public static void onButtonStartClicked() {
+		if(!isStarted){
+			timestamps.add(new Timestamp(new java.sql.Date(System.currentTimeMillis()), TimestampStatus.Gestartet));
+			isStarted = true;
+		}
+	}
+
+	public static void onButtonStopClicked() {
 		timestamps.add(new Timestamp(new java.sql.Date(System.currentTimeMillis()), TimestampStatus.Geschlossen));
+		isStarted = false;
+	}
+
+	public static String timestampsToString() {
+		String returnString = "";
+
+		for (Timestamp timestamp : timestamps) {
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTimeInMillis(Long.parseLong(timestamp.getDate().getTime() + ""));
+
+			returnString += timestamp.getStatus() + "\n" + simpleDateFormat.format(calendar.getTime()) + "\n\n";
+		}
+		return returnString;
 	}
 }
